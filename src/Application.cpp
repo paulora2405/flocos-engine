bc196965f1016f06a376b872a7ba42290d8915a7
Paulo Roberto Albuquerque <paulora2405@gmail.com>
2021-04-29 15:57:47 -0300

    Add GLEW dependency
    Created the main flow for GLFW and GLEW initialization

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,21 +1,67 @@
-#include <GL/freeglut.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 
 #include <iostream>
+#include <string>
 
-short width;
-short height;
+short WIDTH;
+short HEIGHT;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
   if(argc == 1)
-    width = 800, height = 640;
+    WIDTH = 800, HEIGHT = 640;
   else if(argc == 3)
-    width = atoi(argv[1]), height = atoi(argv[2]);
+    WIDTH = atoi(argv[1]), HEIGHT = atoi(argv[2]);
   else {
     std::cerr << "ARGUMENT ERROR" << std::endl;
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
-  std::cout << width << ',' << height << std::endl;
+  GLFWwindow* window;
+
+  /* Initialize GLFW library */
+  if(!glfwInit()) return -1;
+
+  /* Create a windowed mode window and its OpenGL context */
+  std::string window_name{std::to_string(WIDTH) + "," + std::to_string(HEIGHT)};
+  window = glfwCreateWindow(WIDTH, HEIGHT, window_name.c_str(), NULL, NULL);
+  if(!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* Initialize GLEW library */
+  if(glewInit() != GLEW_OK) {
+    std::cerr << "GLEW COULD NOT INITIALIZE" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  /* Print OpenGL Version */
+  std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+  /* Loop until the user closes the window */
+  while(!glfwWindowShouldClose(window)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Test legacy OpenGL */
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
