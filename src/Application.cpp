22488a6a6671931ae6155b1e852d2e0c18e971c1
Paulo Roberto Albuquerque <paulora2405@gmail.com>
2021-05-01 15:45:20 -0300

    Add Logger initialization and configuration

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,8 +8,18 @@
 #include <string>
 
 #include "graphics/graphics_engine.hpp"
+#include "logging/easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char const* argv[]) {
+  /* Logger configuration */
+  START_EASYLOGGINGPP(argc, argv);
+  el::Configurations conf("configuration/logger-default.conf");
+  el::Loggers::reconfigureLogger("default", conf);
+  // el::Loggers::reconfigureAllLoggers(conf);
+
+  //
   unsigned short WIDTH;
   unsigned short HEIGHT;
   if(argc == 1)
