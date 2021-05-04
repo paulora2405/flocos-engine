f6a924aa327106b2d6acb1e9d60bd7b0b7a252ce
Paulo Roberto Albuquerque <paulora2405@gmail.com>
2021-05-04 02:42:28 -0300

    Fix incorrect argument

--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -27,8 +27,8 @@ Texture::Texture(const std::string& filepath)
 
   GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
   GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-  GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP));
-  GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP));
+  GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+  GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
   GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA,
                       GL_UNSIGNED_BYTE, m_LocalBuffer));
