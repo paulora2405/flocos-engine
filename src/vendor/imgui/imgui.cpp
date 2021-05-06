91f6fc8bfef8f1f4e6931aa1409945cd518bb375
Paulo Roberto Albuquerque <paulora2405@gmail.com>
2021-05-05 21:53:26 -0300

    Change configuration file path

--- src/vendor/imgui/imgui.cpp
+++ src/vendor/imgui/imgui.cpp
@@ -1059,7 +1059,7 @@ ImGuiIO::ImGuiIO()
     DisplaySize = ImVec2(-1.0f, -1.0f);
     DeltaTime = 1.0f / 60.0f;
     IniSavingRate = 5.0f;
-    IniFilename = "imgui.ini";
+    IniFilename = "configuration/imgui_conf.ini";// "imgui.ini";
     LogFilename = "imgui_log.txt";
     MouseDoubleClickTime = 0.30f;
     MouseDoubleClickMaxDist = 6.0f;
